7318b547b0dfb9b36e0701b13222c77a6bdfeb91
Pavlo Makarov <pmakarov_tmp@pace.edu>
2016-09-13 18:40:42 -0400

    Delete CallMe.java

--- CallMe.java
+++
@@ -1,9 +0,0 @@
-public class CallMe {
-
-    public static void main(String[] args) {
-        HelloWorld helloWorld = new HelloWorld();
-
-        System.out.println(helloWorld.getWord());
-    }
-}
-
